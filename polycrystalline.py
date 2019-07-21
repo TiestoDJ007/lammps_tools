d04b144854d1c200ad0af0ef90c696c9641626bb
TiestoDJ007 <jackxiao007@outlook.com>
2019-07-21 22:31:25 +0800

    just generate points

--- polycrystalline.py
+++ polycrystalline.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     # 开始生成初始Voronoi 3D结构
     # 规定生成的多面体数量，同时也是输入点的数量
     np.random.seed(1234)
-    point_number = 30
+    point_number = int(30)
     point_cartesian = rand(point_number, 3) * 100
     # 生成初始点
     point_cartesian[:, 0] = point_cartesian[:, 0]
@@ -27,17 +27,18 @@ if __name__ == "__main__":
     # 生成初始Voronoi图
     vor = Voronoi(point_cartesian)
     # 解决维诺图上为无线延展区域
-    new_region_multi = []
+    new_region_multi = {}
     new_vertice_multi = vor.vertices.tolist()
     # 取中心点
     center_point_multi = vor.points.mean(axis=0)
     # 建立所有ridge的地图,将每个区域的表面都与这个区域对应上。数据结构为字典，key为区域编号，value采用tuple.tuple中的第一个数字为与这个区域相连的区域，第二个数字为无限区域所对应的点，顺序可以组成一个封闭的凸平面。
     all_ridges = {}
-    for num_point in range(0, vor.npoints):
+    for num_point in range(len(vor.point_region)):
+        point_region = vor.point_region[num_point]
         region_surface = []
         for num_ridge in range(len(vor.ridge_points)):
             # 判断点是否为无限
-            if num_point == vor.ridge_points[num_ridge][0]:
+            if point_region == vor.ridge_points[num_ridge][0]:
                 # 初始顺序
                 vertices_initial = np.array(vor.ridge_vertices[num_ridge])
                 vertices_length = len(vertices_initial)
@@ -58,8 +59,9 @@ if __name__ == "__main__":
                             ridge_tuple = tuple(ridge_vertices)
                             region_surface.append(ridge_tuple)
                             break
+                all_ridges.setdefault(point_region, region_surface)
             #计算另一组数据，使区域所对应的面完整
-            if num_point == vor.ridge_points[num_ridge][1]:
+            if point_region == vor.ridge_points[num_ridge][1]:
                 vertices_initial = np.array(vor.ridge_vertices[num_ridge])
                 vertices_length = len(vertices_initial)
 
@@ -77,21 +79,20 @@ if __name__ == "__main__":
                             ridge_tuple = tuple(ridge_vertices)
                             region_surface.append(ridge_tuple)
                             break
-            all_ridges.setdefault(num_point, region_surface)
+                all_ridges.setdefault(point_region, region_surface)
     # 重建无限区域
-    for point_0, region in enumerate(vor.point_region):
-        vertice_couple = vor.regions[point_0]
+    for num_region in range(len(vor.point_region)):
+        region_number = vor.point_region[num_region]
+        vertice_couple = vor.regions[region_number]
+        # 挑选出有限区域
         if all(vertice >= 0 for vertice in vertice_couple):
         # 判断是否为有限区域
-            new_region_multi.append(vertice_couple)
+            new_region_multi.setdefault(region_number,tuple(vertice_couple))
             continue
-    # 重新构建无限区域
-        ridge_couple = all_ridges[point_0]
-        new_region = [vertice for vertice in vertice_couple if vertice >= 0]
-
-        for num_surface in range(len(ridge_couple)):
-            if all(point_value >= 0 for point_value in ridge_couple[num_surface]):
+        new_region = [vertice for vertice in vertice_couple if vertice >=0]
+        polyhedron_surface = all_ridges[region_number]
+        for num_polygon in range(len(polyhedron_surface)):
+            polygon_points = polyhedron_surface[num_polygon]
+            if polygon_points[1] != -1:
                 continue
-        # 计算无限区域上的点
-            point_1 = ridge_couple[num_surface][0]
-            t = vor.points[point_1] - vor.points[point_0]
+
